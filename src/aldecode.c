b5e256aa56b2d81b7fd66cd86e6d62e50cd32ff0
Lincoln <linkskill@gmail.com>
2018-06-14 20:25:54 -0300

    fix

--- src/aldecode.c
+++ src/aldecode.c
@@ -9,3 +9,44 @@
 /** Cria o AL Code **/
 #include "alcode.h"
 
+int main(){
+
+    /*
+    2 opções de resolução: 
+     - Tentar achar os 4 quadrados na imagem, que vai servir como alinhamento
+    para ler a região que interessa na imagem (geralmente o centro)
+     - Tentar fazer uma janela deslizante (com o padrão do próprio ALcode) 
+    que vai aumentando a cada iteração "region growing" e ir comparando cada pixel
+    por meio de um threshold 
+    */
+
+    Imagem *img;
+
+    img = abreImagem("../resultados/ALCode.png",1);
+
+    if(!img){
+        printf("No image found!\n");
+        return -1;
+    }
+
+    int mat[img->largura/2][img->altura/2];
+
+    for(int i = 0; i < img->largura/2; i += 1){
+        for(int j = 0; j < img->altura/2; j += 1){
+            if(img->dados[0][i][j] > 0.5f){
+                mat[i][j] = 1;
+                printf("%2d", mat[i][j]);
+            }
+            else{
+                mat[i][j] = 0;
+                printf("%2d", mat[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+
+
+    destroiImagem(img);
+    return 0;
+}
